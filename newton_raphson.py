e5d77b40977f642b42a39345cbd5e9502efeb10b
rotemlv <90067343+rotemlv@users.noreply.github.com>
2024-07-13 22:17:15 +0300

    Included bisection etc

--- newton_raphson.py
+++ newton_raphson.py
@@ -28,7 +28,7 @@ def newtons_method_original(x0, f, f_prime, tolerance, epsilon, max_iterations):
 
     return None                         # Newton's method did not converge
 
-def newtons_method(x0, f, f_prime, tolerance, epsilon, max_iterations):
+def newtons_method(x0, f, f_prime, tolerance, epsilon, max_iterations, do_verbose=False):
     """Newton's method (Wikipedia implementation - modified by me)
     Args:
       x0:              The initial guess
@@ -38,25 +38,21 @@ def newtons_method(x0, f, f_prime, tolerance, epsilon, max_iterations):
       epsilon:         Do not divide by a number smaller than this
       max_iterations:  The maximum number of iterations to compute
     """
+    xxprint = print if do_verbose else (lambda x, sep=None, end=None: None)
     # print(f"Called newtons_method with: {x0=}, {f=}, {f_prime=}, types= {type(x0)}, {type(f)}, {type(f_prime)}")
     assert abs(f.coeffs[0] * f.rank - f_prime.coeffs[0]) < epsilon
     original_guess = x0
     iter_values = []
     for _ in range(max_iterations):
-        try:
-            yprime = f_prime(x0)
-        except RuntimeWarning:
-            print(f"Failed in newton!")
-            exit(-1)
-            # return None
+        yprime = f_prime(x0)
         if abs(yprime) < epsilon:  # Give up if the denominator is too small
-            print("Failed in derivative value!")
+            xxprint("Failed in derivative value (too close to zero)!")
             return None
         try:
             x1 = x0 - f.division_eval(f_prime, x0)  # Do Newton's computation
         except RuntimeWarning as w:
             if "divide by zero" in str(w):
-                print("Failed in derivative value calculation (got 0 in denominator)")
+                xxprint("Failed in derivative value calculation (got 0 in denominator)")
                 return None
 
         if abs(x1 - x0) < tolerance:  # Stop when the result is within the desired tolerance
@@ -65,7 +61,7 @@ def newtons_method(x0, f, f_prime, tolerance, epsilon, max_iterations):
 
         x0 = x1  # Update x0 to start the process again
         iter_values.append(x0)
-    print(f"Failed in max-iterations for {original_guess=}, reached: {x1 if max_iterations>0 else x0}")
+    xxprint(f"Failed in max-iterations for {original_guess=}, reached: {x1 if max_iterations>0 else x0}")
     # print(f"Iteration values: {iter_values[-30:]}")
     # if np.float64(10.191941417363024) == original_guess:
     #     print(iter_values)
